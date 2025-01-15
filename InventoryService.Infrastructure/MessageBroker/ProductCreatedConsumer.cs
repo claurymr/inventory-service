6b5802d734dd4fbe7fb734c757a5209cccebb192
Claury Mejia <clauryfranchesca.mejia@gmail.com>
2025-01-15 12:06:38 -0400

    setup docker and rabbitmq

---
+++ InventoryService.Infrastructure/MessageBroker/ProductCreatedConsumer.cs
@@ -0,0 +1,12 @@
+using InventoryService.Application.Contracts;
+using MassTransit;
+
+namespace InventoryService.Infrastructure.MessageBroker;
+
+public sealed class ProductCreatedConsumer : IConsumer<ProductCreatedEvent>
+{
+    public Task Consume(ConsumeContext<ProductCreatedEvent> context)
+    {
+        throw new NotImplementedException();
+    }
+}
